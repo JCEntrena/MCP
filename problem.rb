206c46a09f68b78b7078c4829e8cbb310c3b6d19
JCEntrena <jentrena@correo.ugr.es>
2017-02-18 22:02:22 +0200

    Definition of a problem: Includes a matrix, a name, and basic data.

---
+++ problem.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+#encoding: utf-8
+
+module Clique
+
+  # This class will store one problem and its data, making it accesible:
+  # Name: name of the file.
+  # nVertices: number of vertices of the graph.
+  # nEdges: number of edges of the graph.
+  # adjacencyMatrix: adjacency matrix. Contains boolean values.
+  class Problem
+
+    attr_reader :name
+    attr_reader :nVertices
+    attr_reader :nEdges
+    attr_reader :adjacencyMatrix
+
+    def initialize(name, nVertices, nEdges, matrix)
+      @name = name
+      @nVertices = nVertices
+      @nEdges = nEdges
+      @adjacencyMatrix = matrix
+    end
+
+    def to_s
+      "Nombre del archivo: #{@name}.
+       Número de vértices: #{@nVertices}.
+       Número de arcos: #{@nEdges}.\n\n"
+    end
+
+  end
+
+end

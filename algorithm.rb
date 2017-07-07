43b7a6831c90e2f8880c67930d1bf3c712b9ab6c
JCEntrena <jentrena@correo.ugr.es>
2017-07-07 22:53:24 +0200

    Añade algoritmo para completar cliques Greedy.

--- algorithm.rb
+++ algorithm.rb
@@ -3,6 +3,10 @@
 
 # Algorithms used for calculations in this project.
 
+require_relative 'localsearch.rb'
+
+include Clique
+
 module Algorithm
 
   public
@@ -31,17 +35,34 @@ module Algorithm
   end
 
   # Returns all nodes connected to a list, except those from the list.
-  # Used for getting the set of all nodes connected to a cliquex
+  # Used for getting the set of all nodes connected to a clique
   def connected_with_all(list, matrix)
     nodes = (0...matrix.size).to_a
     nodes.select!{|y| is_connected(y, list, matrix)}
     nodes - list
   end
 
+  # Returns all nodes connected to all-but-one nodes in a list.
+  # Used for getting the set of all nodes connected to a clique except for one
   def missing_one_connection(list, matrix)
     nodes = (0...matrix.size).to_a
     nodes.select!{|y| connections(y, list, matrix) == (list.length - 1)}
     nodes
+  end
+
+  # Completes a clique using LS
+  # Extends clique with operator ADD until no more vertices can be added.
+  # Uses random addition, as used in operatorADD.
+  def complete_clique(clique, matrix)
+    ls = LocalSearch.new
+    aux = Array.new(clique)
+    element = ls.operatorADD(matrix, aux)
+    until element.nil?
+      aux << element
+      element = ls.operatorADD(matrix, aux)
+    end
+
+    aux
   end
 
 end

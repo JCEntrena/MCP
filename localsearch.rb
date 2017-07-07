de3e0d0a87d39a06ca5ac17e9badd9c62dab1082
JCEntrena <jentrena@correo.ugr.es>
2017-07-07 22:53:17 +0200

    Minor changes

--- localsearch.rb
+++ localsearch.rb
@@ -21,50 +21,47 @@ module Clique
 
     # Operator add.
     # Given a clique and a problem, return a (random) vertex to add to the clique (maintaining the clique structure).
-    def operatorADD(problem, clique)
-      matrix = problem.adjacencyMatrix
-      vertices = (0...problem.nVertices).to_a
+    def operatorADD(matrix, clique)
+      vertices = (0...matrix.length).to_a
 
       # Possible additions set (PA)
       # Using is_connected function, defined in algorithm.rb.
       pAdditions = vertices.select{ |vertex| is_connected(vertex, clique, matrix) } - clique
-
+      # Check if empty
+      if pAdditions.empty?
+        return nil
+      end
       # Return a random element
       pAdditions[@rand.rand(pAdditions.length)]
     end
 
     # Operator swap.
     # Given a clique and a problem, swap two vertices maintaining the clique structure.
-    def operatorSWAP(problem, clique)
-      matrix = problem.adjacencyMatrix
-      vertices = (0...problem.nVertices).to_a
+    def operatorSWAP(matrix, clique)
+      vertices = (0...matrix.length).to_a
 
       # One missing set. Set of vertices connected to all but one nodes in the clique.
       # We assume no vertex in the clique satisfies this condition.
       oneMissing = missing_one_connection(clique, matrix)
+      # Check if empty
+      if oneMissing.empty?
+        return nil
+      end
 
       # Take a random element.
       element = oneMissing[@rand.rand(oneMissing.length)]
 
-      if element.nil?
-        return nil
-      end
-
       # Take the element not connected to the chosen.
       # Taking the first, as it is a one-element array.
       not_connected = clique.delete_if{|vertex| matrix[vertex][element] == 1}.first
-
       # Return pair [out-of-clique, in-clique] to swap.
       [element, not_connected]
     end
 
-    def operatorDROP(problem, clique)
-      matrix = problem.adjacencyMatrix
-      vertices = (0...problem.nVertices).to_a
-
-      # Return node with less connections in the clique.
-      element = clique.min_by{|vertex| connections(vertex, vertices, matrix)}
-      element
+    def operatorDROP(matrix, clique)
+      vertices = (0...matrix.length).to_a
+      # Return node in the clique with less connections overall.
+      clique.min_by{|vertex| connections(vertex, vertices, matrix)}
     end
 
   end

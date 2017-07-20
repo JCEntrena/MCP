e588a4f05a2ec39bc587dbe6199b8100c1138f23
JCEntrena <jentrena@correo.ugr.es>
2017-07-20 11:51:12 +0200

    Memético. Intentar quitar código repetido.

--- genetics.rb
+++ genetics.rb
@@ -45,7 +45,7 @@ module Clique
       nVert = problem.nVertices
       # Params.
       size = 50
-      pMutation = 0.05
+      pMutation = 0.1
       pCruce = 1
       # Initial random population.
       population = []
@@ -111,6 +111,79 @@ module Clique
       puts "Longitud: #{best_clique.length}"
     end
 
+    def solve_memetic(problem, iterations)
+      # Initial declarations
+      matrix = problem.adjacencyMatrix
+      nVert = problem.nVertices
+      # Params.
+      size = 50
+      pMutation = 0.1
+      pCruce = 1
+      # Initial random population.
+      population = []
+      size.times do
+        population << @greedy.solve_random(problem)
+      end
+      # Best clique.
+      best_clique = Array.new(population.max_by{|x| x.length})
+      # Loop
+      iterations.times do |i|
+        new_population = []
+        # Generate new population
+        until population.empty?
+          # Parents
+          parent1 = population.delete_at(@rand.rand(population.length))
+          parent2 = population.delete_at(@rand.rand(population.length))
+          # Cross
+          if @rand.rand() < pCruce
+            all = (parent1 + parent2).uniq
+            common = parent1.select{|x| parent2.include?(x)}
+            not_common = all - common
+            # New generation
+            son1 = Array.new(common)
+            son2 = Array.new(common)
+            not_common.each do |x|
+              if @rand.rand() < 0.5
+                son1 << x
+              else
+                son2 << x
+              end
+            end
+            # Mutation (inversion) (son1)
+            if @rand.rand() < pMutation
+              son1 = mutation(son1)
+            end
+            # Mutation (inversion) (son2)
+            if @rand.rand() < pMutation
+              son2 = mutation(son2)
+            end
+            # Repair sons
+            son1 = @greedy.repair(son1, matrix)
+            son1 = @greedy.complete_clique(son1, matrix)
+            son2 = @greedy.repair(son2, matrix)
+            son2 = @greedy.complete_clique(son2, matrix)
+            # Get two best
+            new_population += [parent1, parent2, son1, son2].max_by(2){|x| x.length}
+          else
+            new_population += [parent1, parent2]
+          end
+        end
+        population = Marshal.load(Marshal.dump(new_population))
+        best = population.max_by{|x| x.length}
+        # Get new best
+        if best.length > best_clique.length
+          best_clique = Array.new(best)
+        end
+
+      end
+      puts "¿Es clique? #{is_clique(best_clique, matrix)}"
+      # Adjust clique, for indexes
+      best_clique.map!{|x| x+1}
+
+      puts "Clique:"
+      puts best_clique.sort
+      puts "Longitud: #{best_clique.length}"
+    end
 
   end
 
